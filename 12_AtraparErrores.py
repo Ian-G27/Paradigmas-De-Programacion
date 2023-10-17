289370247576e14454d81d5712ab155911d72d23
Brokkenguy <fisicsq@gmail.com>
2023-10-17 16:19:18 -0600

    se subio 12

---
+++ 12_AtraparErrores.py
@@ -0,0 +1,82 @@
+#==========================================
+#   Ian Diego Buendia Alvarez
+#==========================================
+#  Matematica Algoritmica 
+#  Paradigmas de Programacion
+#  ESFN IPN octubre 2023
+#==========================================
+
+#=================================================
+#  La clase ClienteBancario esta el subdirectorio
+#  aplicacion/banco/
+#==================================================
+from aplicacion.banco.cliente_bancario import ClienteBancario 
+
+#==================================================
+# try: intenta (correr las instrucciones)
+# except: atrapar el error en una variable
+#  e se puede convertir a string
+#==================================================
+
+#==================================================
+# Error por sacar mas dinero del que tiene
+#==================================================
+try:
+    cliente = ClienteBancario("Jaime Andrade", "Hernandez Sanchez", 28, 0,0)
+    cliente.guardarDinero(300)
+    print(cliente.imprimirInfo())
+    cliente.retirarDinero(400)
+    print(cliente.imprimirInfo())
+#==============================================
+#  Exception es el objeto mas general de error
+#==============================================
+except Exception as e:
+    print("Error: " + str(e))
+
+#================================================
+# Error por usar un atributo privado
+#===============================================
+try: 
+    print(cliente.__nombres)
+except Exception as ex:
+    print("Error: " + str(ex))
+
+#=================================================
+#  Forma correcta
+#=================================================
+print(cliente.nombres)
+
+#=========================================
+#  Clase ClienteBancario
+#=========================================
+class ClienteBancario: 
+    __nombres:str = None
+    __apellidos:str = None
+    __edad:int = 0
+    __balanceDeCuenta:float == 0.0
+    def __init__(self, nombres:str, apellidos:str, edad:int=0, balanceDeCuenta: float=0.0):
+        self.__validarEdad(edad)
+        self.__validarCantidad(balanceDeCuenta)
+        self.nombres = nombres
+        self.apellidos = apellidos
+        self.__edad = edad
+        self.balanceDeCuenta = balanceDeCuenta
+    def getNombreCompleto(self) -> str:
+        return self.nombres + " "+ self.apellidos
+
+    def __mandarEmail(self, titulo:str, texto:str) -> None:
+        print("mandar email: " + titulo + " con texto: " + texto)
+
+    def __enviarBalanceAlBanco(self, cantidad:float) -> None:
+        print("Enviando cantidad: " + str(cantidad) + " al banco...")
+
+        #================================================
+        #  Metodo privado con dos guones bajos
+        #  Si la edad es menhor que 18 genera un error
+        #================================================
+        def __validarEdad(self, edad:int) -> None: 
+            if edad < 18:
+                raise Exception("Es menor de edad")
+        
+        def imprimirInfo(self) -> str:
+            return "Nombre: " + self.getNombreCompleto() + " , Edad: " + str(self.edad) + " , Balance: "+ str(self.__balanceDeCuenta)
